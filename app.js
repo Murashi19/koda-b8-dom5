3165e0c97eb4ed63b77d82ae1670c7d4691a235b
MuhamadRafliShidiq <130623661+MuhamadRafliShidiq@users.noreply.github.com>
2026-06-08 17:05:41 +0700

    doc: add readme.md

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const pass = document.querySelector("input")
+const pass = document.querySelector('.field input[type="password"]')
 const button = document.querySelector("button.btn-eye")
-const svg = document.querySelector("svg")
+const svg = document.querySelector(".btn-eye svg")
 
 const eyeIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye-icon lucide-eye"><path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"/><circle cx="12" cy="12" r="3"/></svg>`
 const offEye = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye-closed-icon lucide-eye-closed"><path d="m15 18-.722-3.25"/><path d="M2 8a10.645 10.645 0 0 0 20 0"/><path d="m20 15-1.726-2.05"/><path d="m4 15 1.726-2.05"/><path d="m9 18 .722-3.25"/></svg>`
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Membuat Navbar dengan fitur toggle hamburger ketika layar mengecil
+
+## Membuat tampilan yang inti membuat button eye di dalam input password dan juga membuat fitur navbar responsive muncul toggle button ketika layar mengecil
+
+hasil implemtasi : ![alt text](img/127.0.0.1_5500_index.html.png)
+
+tampilan ketika mengecil : ![alt text](<img/127.0.0.1_5500_index.html (1).png>)

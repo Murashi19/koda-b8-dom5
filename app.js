2189d87ed24ec40e454228b483a355f5a97354ab
MuhamadRafliShidiq <130623661+MuhamadRafliShidiq@users.noreply.github.com>
2026-06-08 12:06:15 +0700

    feat: add responsive navbar feature that has toggle button feature by implementing DOM

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const pass = document.querySelector("input")
-const button = document.querySelector("button")
+const button = document.querySelector("button.btn-eye")
 const svg = document.querySelector("svg")
 
 const eyeIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye-icon lucide-eye"><path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"/><circle cx="12" cy="12" r="3"/></svg>`
@@ -14,4 +14,9 @@ button.addEventListener("click", function () {
     }
 })
 
-button.appendChild()
+const navbar = document.querySelector(".navbar");
+const toggleBtn = document.querySelector(".btn-toggle");
+
+toggleBtn.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+});
